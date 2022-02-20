f3ae1446b669d3fa3021c7bcd25c7ae18aa1e2ba
Eita,Rami <s76042@beuth-hochschule.de>
2022-02-20 15:43:58 +0100

    init commit

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3"> Coivd -19 Trakcer</div>
+  </header>
+
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
